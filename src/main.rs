41c73918f14e39525789d893e45de7438be646fd
mocrsteel <yannick.thimmesch@gmail.com>
2022-11-04 22:03:19 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
-    println!("Checking trunk.");
-    let x = 14;
-    let y = "testing".to_string();
-    println!("x is {}, {}", x, y);
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(&config.file_path)
+        .expect(&format!("Error while reading file {}", config.file_path)[..]);
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
